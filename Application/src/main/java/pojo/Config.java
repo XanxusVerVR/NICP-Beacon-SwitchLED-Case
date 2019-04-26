71d4d31c55e7e428496d0ef75f7706b0dfde761d
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-04-26 12:17:31 +0800

    給這兩格類別建構子

--- Application/src/main/java/pojo/Config.java
+++ Application/src/main/java/pojo/Config.java
@@ -3,6 +3,9 @@ package pojo;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class Config {
+    public Config() {
+    }
+
     @Override
     public String toString() {
         return new ToStringBuilder(this).toString();
--- Application/src/main/java/pojo/Result.java
+++ Application/src/main/java/pojo/Result.java
@@ -3,6 +3,9 @@ package pojo;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class Result {
+    public Result() {
+    }
+
     @Override
     public String toString() {
         return new ToStringBuilder(this).toString();
